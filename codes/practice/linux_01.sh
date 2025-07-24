b73f5601e201f919679da594c33ea075fe103c73
ParkSeJin <bjtanker_@naver.com>
2025-07-24 17:06:46 +0900

    shell update

---
+++ codes/practice/linux_01.sh
@@ -0,0 +1,31 @@
+#다운로드 경로
+[parksejin@localhost Downloads]$ pwd
+/home/parksejin/Downloads
+[parksejin@localhost Downloads]$ cd /home/parksejin/Downloads
+[parksejin@localhost Downloads]$ pwd
+/home/parksejin/Downloads
+[parksejin@localhost Downloads]$ ls
+[parksejin@localhost Downloads]$ pwd
+/home/parksejin/Downloads
+[parksejin@localhost Downloads]$ cd
+[parksejin@localhost ~]$
+
+#뮤직 경로
+[parksejin@localhost ~]$ pwd
+/home/parksejin
+[parksejin@localhost ~]$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+[parksejin@localhost ~]$ cd Music
+[parksejin@localhost Music]$ pwd
+/home/parksejin/Music
+[parksejin@localhost Music]$ cd
+
+#그림 경로
+[parksejin@localhost ~]$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+[parksejin@localhost ~]$ cd Pictures
+[parksejin@localhost Pictures]$ ls
+[parksejin@localhost Pictures]$ pwd
+/home/parksejin/Pictures
+[parksejin@localhost Pictures]$ cd
+[parksejin@localhost ~]$
